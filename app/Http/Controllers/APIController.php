34d6b396539f174a9bcd298c188ed1253102f3bc
Ben Taber <ben.taber@gmail.com>
2019-08-30 14:23:33 -0600

    remove boilerplate cruft

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Laravel\Lumen\Routing\Controller as BaseController;
 
 const DATA_BEGIN_DATE = '2018/10/01';
 const DATE_FORMAT = 'Y-m-d';
 const ALL_STATIONS = 'all';
 
-class APIController extends Controller
+class APIController extends BaseController
 {
     public function sites()
     {
@@ -46,20 +47,14 @@ class APIController extends Controller
         $cacheKey = join('_', [$stationId,$startDate,$count]);
 
         $measurements = Cache::rememberForever($cacheKey, function() use ($startDate, $endDate, $stationId) {
-            $query = '
+            $query = sprintf('
                 select measurement_date, station_id, snow_depth
                 from snotel_measurements where measurement_date between ? and ?
-                order by measurement_date asc
-            ';
+                %s
+                order by measurement_date asc;
+            ', (ALL_STATIONS != $stationId) ? ' and station_id = ?' : '');
 
-            if (ALL_STATIONS != $stationId) {
-                $query .= ' and station_id = ?';
-                $data = DB::select($query, [$startDate, $endDate, $stationId]);
-            }
-            else {
-                $data = DB::select($query, [$startDate, $endDate]);
-            }
-
+            $data = DB::select($query, [$startDate, $endDate, $stationId]);
             $json = [];
 
             // flip to json structure { 'stationid': [{ date: date, val: val }] }
@@ -72,7 +67,7 @@ class APIController extends Controller
                 ]);
             }
 
-            return $json;
+            return (object)$json;
         });
 
         return response()->json($measurements);
