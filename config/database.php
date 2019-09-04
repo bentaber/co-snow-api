8660b63fcd30add916f4b0fb3faae1364ac4b10c
Ben Taber <ben.taber@gmail.com>
2019-09-04 13:44:38 -0600

    swap to DATABASE_URL style in all envs

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+// parse env var standard DB URL format if present
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"];
+$port = $url["port"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -35,11 +43,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 5432),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $host),
+            'port' => env('DB_PORT', $port),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => env('DB_PREFIX', ''),
             'schema' => env('DB_SCHEMA', 'public'),
